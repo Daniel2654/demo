246f26d881dc8c4272fcf8b600d392e278c078b6
danie <danielalfia99@gmail.com>
2022-11-08 20:50:56 +0200

    created personal web for charlie

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
-contactButton = document.querySelector("#contact button");
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
-contactButton.addEventListener("click", function contactClickhandler(el) {
-  alert("Not implemented");
-});
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+flag = 0;
+
+function changeColor()
+{
+
+  flag = !flag;
+  if (flag)
+  {
+    img = "url('./images/background1.jpg')"
+  } 
+  else
+  {
+    img = "url('./images/background2.jpg')"
+  }
+  alert("Hello world!");
+
+  document.body.style.background = img;
+  var all = document.getElementsByTagName("*");
+  for (var i=0, max=all.length; i < max; i++) {
+    var randomColor = Math.floor(Math.random()*16777215).toString(16);
+    all[i].style.color = randomColor;
+  }
+}
+
